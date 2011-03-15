0329696ee015e26e2fc6b2fb407a505cb6cb0315
josue <jfigueroagama@redesvozdatos.com>
2011-03-14 20:33:33 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CarmenGallery::Application.config.secret_token = '8c994515e3674e93c7dc4632d4a772fe92408b360b3fea5817944773f4e7c7dc7fb2273a1652dfc27118c88211713ab9a9501e3a51d76a02043fadba84adec48'
