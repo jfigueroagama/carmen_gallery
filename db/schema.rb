a7bcdd633c63c649622d9ac505eeba372a9b28d4
josue <jfigueroagama@redesvozdatos.com>
2011-04-26 21:27:10 -0500

    Done with users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110407011032) do
+ActiveRecord::Schema.define(:version => 20110425231210) do
 
   create_table "artworks", :force => true do |t|
     t.string   "name"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(:version => 20110407011032) do
     t.datetime "updated_at"
     t.string   "encrypted_password"
     t.string   "salt"
+    t.boolean  "admin",              :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -50,7 +50,7 @@ describe "Users" do
         fill_in :password, :with => user.password
         click_button
         controller.should be_signed_in
-        click_link "Admin Sign out"
+        click_link "Sign out"
         controller.should_not be_signed_in
       end
     end
