593b9b84c8c0d0848755de0f37393136a8e90047
josue <jfigueroagama@redesvozdatos.com>
2011-04-27 07:11:49 -0500

    Done deleting faker

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 # == Schema Information
-# Schema version: 20110407011032
+# Schema version: 20110425231210
 #
 # Table name: users
 #
@@ -10,6 +10,7 @@
 #  updated_at         :datetime
 #  encrypted_password :string(255)
 #  salt               :string(255)
+#  admin              :boolean(1)
 #
 
 require 'digest'
